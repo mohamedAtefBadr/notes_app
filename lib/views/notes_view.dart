0493b9f76441408e7b3af207e35adf5ef602011b
mohamedAtefBadr <atefm0790@gmail.com>
2023-03-13 23:46:35 +0200

    custom_app_bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../widgets/notes_view_body.dart';
+
 class Notes_View extends StatelessWidget {
   const Notes_View({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
